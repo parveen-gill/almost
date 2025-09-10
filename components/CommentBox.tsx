95aef362eb96340e99879fd3d020f03f6d27f712
parveen-gill <parveen@netgains.org>
2025-09-10 17:44:53 +0530

    add page

---
+++ components/CommentBox.tsx
@@ -0,0 +1,8 @@
+export default function NotificationsPage() {
+  return (
+    <div>
+      <h1>Comment</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
---
+++ components/ProfileCard.tsx
@@ -0,0 +1,8 @@
+export default function NotificationsPage() {
+  return (
+    <div>
+      <h1>Profile Card</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
